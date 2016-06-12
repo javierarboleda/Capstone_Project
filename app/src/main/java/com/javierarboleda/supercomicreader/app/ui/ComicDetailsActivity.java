1131281b7e731adf00c7cef0ed45f102c6178294
Javier Arboleda <javier.g.arboleda@gmail.com>
2016-06-12 01:16:56 -0700

    feat: Add comic cover image, title, flat buttons to comic details

--- app/src/main/java/com/javierarboleda/supercomicreader/app/ui/ComicDetailsActivity.java
+++ app/src/main/java/com/javierarboleda/supercomicreader/app/ui/ComicDetailsActivity.java
@@ -1,14 +1,22 @@
 package com.javierarboleda.supercomicreader.app.ui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.PointF;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
 
+import com.davemorrissey.labs.subscaleview.ImageSource;
+import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
 import com.javierarboleda.supercomicreader.R;
 import com.javierarboleda.supercomicreader.app.model.Comic;
 
 public class ComicDetailsActivity extends AppCompatActivity {
 
     Comic mComic;
+    SubsamplingScaleImageView mBackgroundImageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +24,57 @@ public class ComicDetailsActivity extends AppCompatActivity {
 
         mComic = getIntent().getParcelableExtra("comic");
 
+        String coverFilePath = Environment.getExternalStorageDirectory() +
+                mComic.getFile() + mComic.getCover();
+
         setContentView(R.layout.activity_comic_details);
+
+
+
+        TextView titleTextView = (TextView) findViewById(R.id.title_textView);
+
+        titleTextView.setText(mComic.getTitle());
+
+
+
+        mBackgroundImageView = (SubsamplingScaleImageView) findViewById(R.id.background);
+
+        mBackgroundImageView.setImage(ImageSource.uri(coverFilePath));
+
+        mBackgroundImageView.setPanEnabled(false);
+
+        mBackgroundImageView.setZoomEnabled(false);
+
+        mBackgroundImageView.setOnImageEventListener(new SubsamplingScaleImageView.OnImageEventListener() {
+            @Override
+            public void onReady() {}
+
+            @Override
+            public void onImageLoaded() {
+                int sWidth = mBackgroundImageView.getSWidth();
+                int width = mBackgroundImageView.getWidth();
+
+                Log.d("image", sWidth + " " + width);
+
+                mBackgroundImageView.setScaleAndCenter(
+                        ((float) mBackgroundImageView.getWidth()) / ((float) mBackgroundImageView.getSWidth()),
+                        new PointF(mBackgroundImageView.getWidth()/2, 0)
+                );
+            }
+
+            @Override
+            public void onPreviewLoadError(Exception e) {}
+
+            @Override
+            public void onImageLoadError(Exception e) {}
+
+            @Override
+            public void onTileLoadError(Exception e) {}
+        });
+
+
     }
+
+
+
 }
